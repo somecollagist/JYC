d2f0a8c670fef7d0de66c9a44f8d2265a5d7a3bf
Alex Davies <alex@frogtrade.com>
2022-03-09 23:07:39 +0000

    Added Option Type and Physics2D class for physics helper functions

---
+++ JYCEngine/Lib/Option.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JYCEngine.StdLib;
+public struct Option<T>
+{
+    public static Option<T> None => default;
+    public static Option<T> Some(T value) => new Option<T>(value);
+
+    readonly bool isSome;
+    readonly T value;
+
+    Option(T value)
+    {
+        this.value = value;
+        isSome = this.value is { };
+    }
+
+    public bool IsSome(out T value)
+    {
+        value = this.value;
+        return isSome;
+    }
+}
---
+++ JYCEngine/Lib/Physics2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JYCEngine.StdLib;
+
+public static class Physics2D
+{
+    public static Option<IntersectionPoint> LineLineIntersection(Vector2 aFrom, Vector2 aTo, Vector2 bFrom, Vector2 bTo)
+    {
+        var a = aFrom;
+        var b = aTo - aFrom;
+        var c = bFrom;
+        var d = bTo - bFrom;
+
+        var m = (c - a).Cross(b) / b.Cross(d);
+        var l = (a - c).Cross(d) / d.Cross(b);
+
+        if (0 <= l && l <= 1 && 0 <= m && m <= 1) return Option<IntersectionPoint>.None;
+
+        return Option<IntersectionPoint>.Some(new()
+        {
+            point = a + m * b,
+            normal = b.Normal().Normalize()
+        });
+    }
+
+    public struct IntersectionPoint
+    {
+        public Vector2 point;
+        public Vector2 normal;
+    }
+}
